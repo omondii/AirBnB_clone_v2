23b265e39b37096de4a24502088715d4291cb223
Brian Omondi <omondiibrian00@gmail.com>
2023-09-20 22:29:44 +0300

    User mapped. We have data from the server!

--- console.py
+++ console.py
@@ -148,6 +148,7 @@ class HBNBCommand(cmd.Cmd):
 
             new_instance.save()
             print(new_instance.id)
+            storage.save()
 
     def help_create(self):
         """ Help information for the create method """
