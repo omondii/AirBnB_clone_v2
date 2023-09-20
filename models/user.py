4833cd321895d902b6e3b4825d12a183896128fd
Brian Omondi <omondiibrian00@gmail.com>
2023-09-21 00:16:06 +0300

    Update place

--- models/user.py
+++ models/user.py
@@ -8,11 +8,19 @@ from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes
-    Params: 
+    Params:
+      __tablename__ class attr reping users table
+      email - column containing a string (128 characters)
+      password - column containing a string (128 characters)
+      first_name - column containing a string (128 characters)
+      last_name - column containing a string (128 characters)
     """
 
     __tablename__ = 'users'
+
     email = Column(String(128), nullable=True)
     password = Column(String(128), nullable=True)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+
+    places = relationship('Place', cascade='delete', back_populates='user')
