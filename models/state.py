901479a6d1d4f36a0f9dbf8ed9db3ac3f069d81d
Brian Omondi <omondiibrian00@gmail.com>
2023-09-22 01:05:10 +0300

    error is due to parsing of args

--- models/state.py
+++ models/state.py
@@ -11,25 +11,25 @@ from os import getenv
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-
-    # Define relationships for DBStorage
-    cities = relationship('City', back_populates='state',
-                          cascade='all, delete-orphan')
-
-
-    # Getter att for FileStorage
-    # If the env is not HB..- app doesn't use any db for storage this
-    # is executed
     if getenv("HBNB_TYPE_STORAGE") != "db":
-        @property
-        def cities(self):
-            """
-            Returns a list of city instances with state_id equal to
-            current state.id
-            """
-            cityList = []
-            for city in list(models.storage.all(City).values()):
-                if city.state_id == self.id:
-                    cityList.append(city)
+        name = Column(String(128), nullable=False)
+
+        # Define relationships for DBStorage
+        cities = relationship('City', back_populates='state',
+                              cascade='all, delete-orphan')
+
+
+        # Getter att for FileStorage
+        # If the env is not HB..- app doesn't use any db for storage this
+        # is executed
+    @property
+    def cities(self):
+        """
+        Returns a list of city instances with state_id equal to
+        current state.id
+        """
+        cityList = []
+        for city in list(models.storage.all(City).values()):
+            if city.state_id == self.id:
+                cityList.append(city)
             return cityList
