cbe053ece64d01213e172cce4ab429b4d21e2664
Brian Omondi <omondiibrian00@gmail.com>
2023-10-19 23:59:05 +0300

    Getting started with flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Imported modules:
+flask
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run()
