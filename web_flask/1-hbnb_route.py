bb3596a8cf638a62256bc2ccaefc056656d84345
Brian Omondi <omondiibrian00@gmail.com>
2023-10-20 09:25:59 +0300

    pystyle check

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -7,13 +7,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello():
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hnbn():
     return "HBNB"
+
 
 if __name__ == '__main__':
     app.run()
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -7,18 +7,22 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello():
     return "Hello HBNB!"
+
 
 @app.route("/hbnb", strict_slashes=False)
 def hnbn():
     return "HBNB"
 
+
 @app.route("/c/<text>", strict_slashes=False)
 def text(text):
-    text = text.replace("_"," ")
+    text = text.replace("_", " ")
     return f"C {text}"
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
